8e48cb5572fe5acc0c3e65fbfc01761d5ce35b01
Максим Хорошеньков <maksimhorosenkov@gmail.com>
2025-06-21 13:09:05 +0300

    PKI-2742: добавил бенчмарки

---
+++ test/BenchmarksRunnerCli/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarks;
+
+namespace BenchmarksRunnerCli;
+
+public static class Program
+{
+    private static void Main(string[] args) 
+        => BenchmarkSwitcher.FromAssembly(typeof(QueueProvidersBenchmarks).Assembly).Run(args);
+}
